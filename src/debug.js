133d130e806325b99940dc54db702667a089df23
SrAquaGenius <dimoisivic@gmail.com>
2025-06-03 21:02:51 +0100

    feat: função debug agora indica em que função foi chamada

--- src/debug.js
+++ src/debug.js
@@ -5,17 +5,34 @@
 
 let debugAtivo = false;
 
+/** Ativa a saída de debug. */
 function ativarDebug() {
 	debugAtivo = true;
 }
 
+/** Desativa a saída de debug. */
 function desativarDebug() {
 	debugAtivo = false;
 }
 
+/**
+ * Imprime mensagens de debug, indicando a função de chamada.
+ * @param {...any} mensagens Mensagens a imprimir em modo debug.
+ */
 function debug(...mensagens) {
+
 	if (debugAtivo) {
-		console.log("[DEBUG]", ...mensagens);
+		const stack = new Error().stack;
+		const linhas = stack.split("\n");
+		let funcao = "desconhecida";
+
+		// Tenta extrair o nome da função chamadora
+		if (linhas.length >= 3) {
+			const match = linhas[2].match(/at (\S+)/);
+			if (match) funcao = match[1];
+		}
+
+		console.log(`[DEBUG][${funcao}]`, ...mensagens);
 	}
 }
 
